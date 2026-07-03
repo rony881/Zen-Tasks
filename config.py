8fde60ac30366304e8fc222334f2db4ae97c8ede
Rony <ronybiswasinnovative@gmail.com>
2026-07-03 20:49:35 +0600

    Improved config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ This File Contains all constants and settings used throughout the Application
 """
 
 from pathlib import Path
+from datetime import datetime
 
 # ======== Apllication Data ================
 DAYS = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
@@ -26,8 +27,26 @@ UI_CONFIG = {
     "dialog_width": 600,
     "dialog_height": 300,
 }
+TABLE_CONFIG = {
+    "time_col_width" : 130,
+    "priority_col_width" : 150,
+}
 
 # ============ Table Configuration ============
 TIME_COL = 0
 TASK_COL = 1
 PRIORITY_COL = 2
+
+# ============ UI Styling Constants ============
+INFO_BAR_DURATION_SHORT = 1800
+INFO_BAR_DURATION_LONG = 2000
+TABLE_ROW_HEIGHT = 46
+
+# ============ Color Constants ============
+PRIORITY_COLOR = "#ca2851"
+PRIMARY_COLOR = "#2383E2"
+PRIMARY_COLOR_HOVER = "#1A73CE"
+PRIMARY_COLOR_PRESSED = "#1260B5"
+
+# ==== Current Day ======
+current_day = datetime.now().strftime("%a")
