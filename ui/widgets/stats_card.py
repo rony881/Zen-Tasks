0fb5aac9294456624300d9cebe0dd45b949ea8d8
Rony <ronybiswasinnovative@gmail.com>
2026-07-10 20:59:49 +0600

    Updated stats_card.py/StatsCard

--- ui/widgets/stats_card.py
+++ ui/widgets/stats_card.py
@@ -1,25 +1,42 @@
 from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout
-from qfluentwidgets import CaptionLabel, CardWidget,StrongBodyLabel
+from qfluentwidgets import CaptionLabel, CardWidget, IconWidget,StrongBodyLabel
 
 
 class StatsCard(CardWidget):
-    def __init__(self, parent,title,value,subtitle):
+    def __init__(self, parent, icon, title: str, value, subtitle: str):
         super().__init__(parent)
         # self.setFixedSize(270, 170)
         
         layout = QVBoxLayout(self)
+        layout.setContentsMargins(16, 14, 16, 12)
+        layout.setSpacing(4)
         
+        # ========= Top Area ========
         top = QHBoxLayout()
-        titleLabel = CaptionLabel(title)
-        top.addWidget(titleLabel)
-        top.addStretch()
+        top.setSpacing(4)
 
-        valueLabel = StrongBodyLabel(value)
-        valueLabel.setStyleSheet("font-size:32px;color:#555;")
-        
-        subtitleLabel = CaptionLabel(subtitle)
+        # -------- Icon ---------
+        icon_w = IconWidget(icon, self)
+        icon_w.setFixedSize(15, 15)
+        top.addWidget(icon_w)
+
+        # -------- Title --------
+        titleLabel = CaptionLabel(title, self)
+        top.addWidget(titleLabel)
+        top.addStretch(1)
 
         layout.addLayout(top)
+
+        # ------- Value Label ------
+        valueLabel = StrongBodyLabel(value,self)
+        valueLabel.setStyleSheet("""
+            font-size:25px;
+            color:#3a3a3a;
+            """)
         layout.addWidget(valueLabel)
-        layout.addWidget(subtitleLabel)
         
+        subLabel = CaptionLabel(subtitle,self)
+        subLabel.setTextColor("#606060", "#c0c0c0")
+        layout.addWidget(subLabel)
+        layout.addStretch(1)
+                
