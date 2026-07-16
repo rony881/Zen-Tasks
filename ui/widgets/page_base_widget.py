2ef9fd0fb06cbe0d728d8cbf055280508fc65032
Rony <ronybiswasinnovative@gmail.com>
2026-07-16 23:00:59 +0600

    Update page_base_widget.py

--- ui/widgets/page_base_widget.py
+++ ui/widgets/page_base_widget.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QFrame, QHBoxLayout, QVBoxLayout, QWidget
 from qfluentwidgets import FluentIcon, PrimaryPushButton, TitleLabel
 from ui.theme import ADD_BTN_STYLE, TITLE_STYLE
+from ui.widgets.add_task_dialog import AddTaskDialog
 
 
 class PageBaseWidget(QWidget):
@@ -11,18 +12,19 @@ class PageBaseWidget(QWidget):
         self.page_layout = QVBoxLayout(self)
         self.page_layout.setContentsMargins(24, 1, 24, 24)
 
+    def setPageType(self,type):
+        self.page_type = type
+        
     def setPageHeader(self,header: str, button: str | None = None): 
         h_lout = QHBoxLayout(self)
-        
         h = TitleLabel(self,header)
         h.setStyleSheet(TITLE_STYLE)
         h_lout.addWidget(h)
         h_lout.addStretch(1)
-
         if button is not None:
             self.add_btn = PrimaryPushButton(FluentIcon.ADD,"    " + button)
             self.add_btn.setStyleSheet(ADD_BTN_STYLE)
-            self.add_btn.clicked.connect(self.show_input_dialog)
+            self.add_btn.clicked.connect(self.showInputDialog)
             h_lout.addWidget(self.add_btn)
             
         self.page_layout.addLayout(h_lout)
@@ -30,5 +32,14 @@ class PageBaseWidget(QWidget):
     def AddWidget(self,widget):
         self.page_layout.addWidget(widget)
 
-    def show_input_dialog(self):
-        ...
+    def showInputDialog(self):
+        match self.page_type:
+            case "weekly":
+                dialog = AddTaskDialog(self)
+                return dialog
+            case "today":
+                dialog = AddTaskDialog(self)
+                return dialog
+            case "sleep tracker":
+                dialog = AddTaskDialog(self)
+                return dialog 
