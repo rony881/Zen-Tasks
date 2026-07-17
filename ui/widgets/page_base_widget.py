7d90b50dcb48332f341656ba9abfdd454496eb3c
Rony <ronybiswasinnovative@gmail.com>
2026-07-17 18:40:18 +0600

    Update page_base_widget.py

--- ui/widgets/page_base_widget.py
+++ ui/widgets/page_base_widget.py
@@ -1,7 +1,6 @@
-from PyQt6.QtWidgets import QFrame, QHBoxLayout, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout, QWidget
 from qfluentwidgets import FluentIcon, PrimaryPushButton, TitleLabel
 from ui.theme import ADD_BTN_STYLE, TITLE_STYLE
-from ui.widgets.add_task_dialog import AddTaskDialog
 
 
 class PageBaseWidget(QWidget):
@@ -16,30 +15,33 @@ class PageBaseWidget(QWidget):
         self.page_type = type
         
     def setPageHeader(self,header: str, button: str | None = None): 
-        h_lout = QHBoxLayout(self)
-        h = TitleLabel(self,header)
-        h.setStyleSheet(TITLE_STYLE)
-        h_lout.addWidget(h)
-        h_lout.addStretch(1)
+        self.page_layout.addLayout(self.headerFrame(header, button))
+
+    def headerFrame(self,header: str, button: str | None = None):
+        header_lout = QHBoxLayout(self)
+        
+        hdr = TitleLabel(header)
+        hdr.setStyleSheet(TITLE_STYLE)
+        header_lout.addWidget(hdr)
+        header_lout.addStretch(1)
+        
         if button is not None:
             self.add_btn = PrimaryPushButton(FluentIcon.ADD,"    " + button)
             self.add_btn.setStyleSheet(ADD_BTN_STYLE)
-            self.add_btn.clicked.connect(self.showInputDialog)
-            h_lout.addWidget(self.add_btn)
-            
-        self.page_layout.addLayout(h_lout)
-        
-    def AddWidget(self,widget):
-        self.page_layout.addWidget(widget)
+            self.add_btn.clicked.connect(self.onAddButtonClicked)
+            header_lout.addWidget(self.add_btn)
 
-    def showInputDialog(self):
-        match self.page_type:
-            case "weekly":
-                dialog = AddTaskDialog(self)
-                return dialog
-            case "today":
-                dialog = AddTaskDialog(self)
-                return dialog
-            case "sleep tracker":
-                dialog = AddTaskDialog(self)
-                return dialog 
+        return header_lout
+        
+    def addWidget(self,widget: QWidget):
+        self.page_layout.addWidget(widget)
+        
+    def addLayout(self,layout):
+        self.page_layout.addLayout(layout)
+
+    def onAddButtonClicked(self):
+        """
+        Override this method to decide what happen 
+        if add button is clicked
+        """
+        ...
