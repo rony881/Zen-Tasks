11230703029fcdff5a67c445699bf8c03290e7ce
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-14 14:20:30 +0600

    refactor: remove dead code.

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ from pathlib import Path
 from datetime import datetime
 
 # ======== Apllication Data ================
-DAYS = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
 PRIORITIES = ["Low", "Medium", "High", "Critical"]
 
 # ============ Directory Paths ============
@@ -16,10 +15,7 @@ DATA_DIR = BASE_DIR / "data"
 
 # ============ Data Files ============
 DB_FILE = DATA_DIR / "Zen.db"
-WEEKLY_SCHEDULE_FILE = DATA_DIR / "weekly_schedule.json"
-TODAYS_TASKS_FILE = DATA_DIR / "todays_tasks.json"
-LAST_LOADED_DAY_FILE = DATA_DIR / "last_loaded_day.txt"
-SLEEP_LOGS_FILE = DATA_DIR / "sleep_logs.json"
+
 
 # ============ UI Configuration ============
 UI_CONFIG = {
@@ -30,10 +26,7 @@ UI_CONFIG = {
     "dialog_width": 600,
     "dialog_height": 300,
 }
-TABLE_CONFIG = {
-    "time_col_width" : 130,
-    "priority_col_width" : 150,
-}
+
 
 # ============ Table Configuration ============
 TIME_COL = 0
@@ -42,16 +35,8 @@ PRIORITY_COL = 2
 
 # ============ UI Styling Constants ============
 INFO_BAR_DURATION_SHORT = 1800
-INFO_BAR_DURATION_LONG = 2000
 TABLE_ROW_HEIGHT = 46
-PRIMARAY_FONT = 16
-SECONDARY_FONT = 13
 
-# ============ Color Constants ============
-PRIORITY_COLOR = "#ca2851"
-PRIMARY_COLOR = "#2383E2"
-PRIMARY_COLOR_HOVER = "#1A73CE"
-PRIMARY_COLOR_PRESSED = "#1260B5"
 
 # ==== Current Day ======
 current_day = datetime.now().strftime("%a")
