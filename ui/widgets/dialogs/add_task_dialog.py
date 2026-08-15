5bf998a33e7abbf2f90618d1defb3423266d4c37
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-15 23:27:54 +0600

    rafector: migrate AddTaskDialog from QDialog to MessageBoxBase.

--- ui/widgets/dialogs/add_task_dialog.py
+++ ui/widgets/dialogs/add_task_dialog.py
@@ -1,7 +1,7 @@
 from qfluentwidgets import InfoBar, InfoBarPosition
 from config import INFO_BAR_DURATION_SHORT, PRIORITIES,UI_CONFIG
 from core.models.task import Task
-from ui.theme import PRIORITY_STYLE, TASK_INPUT_STYLE
+from ui.theme import ADD_BTN_STYLE, PRIORITY_STYLE, TASK_INPUT_STYLE
 from ui.widgets.base_widgets.dialog_base_widget import DialogBaseWidget
 DIALOG_WIDTH = UI_CONFIG["dialog_width"]
 DIALOG_HEIGHT = UI_CONFIG["dialog_height"]
@@ -12,26 +12,38 @@ class AddTaskDialog(DialogBaseWidget):
     def _setup_ui(self):
         """Set up the dialog UI components."""
         self.setDialogTitle("Add New Task")
-        self.setDialogSize()
         
+        # Task input
         self.task_input = self.makeTextArea(
             "write task discription...",
             110,
             TASK_INPUT_STYLE
         )
+        self.add(self.makeLabel("Task Name"))
         self.add(self.task_input)
-        
-        self.timePicker = self.makeTimePicker()
-        self.priority = self.makeComboBox(PRIORITIES, "priority", PRIORITY_STYLE)
 
-        footer = self.makeFooter(
-                self.timePicker,
-                self.priority,
-                submitBtnText= "Add Task"
-            )
-        self.add(footer)
+        # Time picker
+        time_picker_layout = self.makeHLayout()
+        self.timePicker = self.makeTimePicker()
+        time_picker_layout.addWidget(self.makeLabel("Time :"))
+        time_picker_layout.addWidget(self.timePicker)
+        self.add(time_picker_layout)
+
+        # Priority
+        priority_layout = self.makeHLayout()
+        self.priority = self.makeComboBox(PRIORITIES, "priority", PRIORITY_STYLE)
+        priority_layout.addWidget(self.makeLabel("Priority :"))
+        priority_layout.addWidget(self.priority)
+        self.add(priority_layout)
         
-    def onSubmit(self) -> None:
+        self.setSubmitButtonText(
+            "Add Task",
+            object_name="add_task_button",
+            stylesheet=ADD_BTN_STYLE
+        )
+        
+    def validate(self) -> bool:
+        """Validate the sleep entry before submitting."""
         task = self.task_input.toPlainText().strip()
         priority = self.priority.currentText()
     
@@ -43,9 +55,8 @@ class AddTaskDialog(DialogBaseWidget):
                 position=InfoBarPosition.TOP,
                 parent=self,
             )
-            return
-    
-        self.accept()
+            return False
+        return True
 
     def get_data(self):
         time = self.timePicker.getTime().toString("hh:mm AP")
